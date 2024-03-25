dc41ad26e759589ab9e2c3db318de3540f909408
MartinGE01 <martinmartin01g@gmail.com>
2024-03-25 15:56:18 -0500

    dashboar_f

--- resources/views/user/usuario.blade.php
+++ resources/views/user/usuario.blade.php
@@ -1,6 +1,10 @@
 @extends('raiz.panel')
 
+
 @section('content')
+
+
+
 <div class="card mb-4">
     <div class="card-header">
         <i class="fas fa-table mr-1"></i>
@@ -20,43 +24,72 @@
                     </tr>
                 </thead>
                 <tbody>
-                    {{-- @foreach($customers as $row) --}}
+                  
                     <tr>
-                        <td>name</td>
-                        <td>email</td>
-                        <td>Company</td>
-                        <td>Address</td>
-                        <td>Phone</td>
+                        <td>name </td>
+                        <td>email </td>
+                        <td>company </td>
+                        <td>address </td>
+                        <td>phone </td>
                         <td>
-                            {{-- Include action buttons here --}}
+                          
                         </td>
                     </tr>
-                    {{-- @endforeach --}}
+               
+                    
                 </tbody>
             </table>
         </div>
     </div>
 </div>
 @endsection
-
 @section('script')
-<script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
-<script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
 <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js" crossorigin="anonymous"></script>
-<script src="https://cdn.datatables.net/buttons/1.7.1/js/dataTables.buttons.min.js" crossorigin="anonymous"></script>
-<script src="https://cdn.datatables.net/buttons/1.7.1/js/buttons.bootstrap4.min.js" crossorigin="anonymous"></script>
-<script src="https://cdn.datatables.net/buttons/1.7.1/js/buttons.html5.min.js" crossorigin="anonymous"></script>
-<script src="https://cdn.datatables.net/buttons/1.7.1/js/buttons.print.min.js" crossorigin="anonymous"></script>
+        
 <script>
-    $(document).ready(function () {
-        $('#dataTable').DataTable({
-            dom: 'lBfrtip',
-            buttons: [
-                'copy', 'excel', 'pdf', 'print'
-            ]
-        });
-    });
-</script>
+   
+
+
+   $('#dataTable').DataTable({
+    columnDefs: [
+    {bSortable: false, targets: [5]} 
+  ],
+                dom: 'lBfrtip',
+           buttons: [
+               {
+                   extend: 'copyHtml5',
+                   exportOptions: {
+                    modifier: {
+                        page: 'current'
+                    },
+                       columns: [ 0, ':visible' ]
+                       
+                   }
+               },
+               {
+                   extend: 'excelHtml5',
+                   exportOptions: {
+                    modifier: {
+                        page: 'current'
+                    },
+                    columns: [ 0, ':visible' ]
+                   }
+               },
+               {
+                   extend: 'pdfHtml5',
+                   exportOptions: {
+                    modifier: {
+                        page: 'current'
+                    },
+                       columns: [ 0, 1, 2, 5 ]
+                   }
+               },
+
+               'colvis'
+
+           ],
+            
+           });
+       </script>
+
 @endsection
