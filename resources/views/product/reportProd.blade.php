c644ac4bfd39806e7934acb08079565301daa305
MartinGE01 <martinmartin01g@gmail.com>
2024-03-24 23:40:35 -0500

    dashboar_d

--- resources/views/product/reportProd.blade.php
+++ resources/views/product/reportProd.blade.php
@@ -1,8 +1,76 @@
 @extends('raiz.panel')
 
-
-
 @section('content')
-<h1>Reporte de productos</h1>
+    <h1>Reporte de productos</h1>
+    <div class="card mb-4">
+        <div class="card-header">
+            <i class="fas fa-table mr-1"></i>
+            Products in Stock
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>Code</th>
+                            <th>Name</th>
+                            <th>Category</th>
+                            <th>Stock</th>
+                            <th>Unit Price</th>
+                            <th>Sale Price</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>producto</td>
+                            <td>nombtre</td>
+                            <td>categoria</td>
+                            {{-- Comentario sobre la gestión del stock --}}
+                            <td>stock</td>
+                            <td>unit_price</td>
+                            <td>sales_unit_price</td>
+                            <td>
+                                <a href="#" class="btn btn-sm btn-info">Edit</a>
+                                <a href="#" class="btn btn-sm btn-danger">Delete</a>
+                                {{-- Botón Purchase --}}
+                                <a href="#" class="btn btn-sm btn-info">Purchase</a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+@endsection
 
+@section('script')
+    <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
+    {{-- DataTables Buttons --}}
+    <script src="https://cdn.datatables.net/buttons/1.7.1/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.7.1/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.7.1/js/buttons.print.min.js"></script>
+
+    <script>
+        $(document).ready(function() {
+            $('#dataTable').DataTable({
+                columnDefs: [
+                    { targets: [6], orderable: false } // Desactivar la ordenación en la columna de acciones
+                ],
+                dom: 'lBfrtip',
+                buttons: [
+                    'copyHtml5',
+                    'excelHtml5',
+                    'pdfHtml5',
+                    'print'
+                ]
+            });
+        });
+    </script>
 @endsection
--- resources/views/raiz/panel.blade.php
+++ resources/views/raiz/panel.blade.php
@@ -148,9 +148,14 @@
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
         <script src="{{ asset('backend') }}/js/scripts.js"></script>
+
+
+        <script src="{{ asset('js/scripts.js') }}"></script>
+        <script src="{{ asset('js/demo/chart-area-demo.js') }}"></script>
+        <script src="{{ asset('js/demo/chart-bar-demo.js') }}"></script>
+
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-        <script src="{{ asset('backend') }}/assets/demo/chart-area-demo.js"></script>
-        <script src="{{ asset('backend') }}/assets/demo/chart-bar-demo.js"></script>
+       
         <!-- <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
         <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script> -->
          <!-- <script src="{{ asset('backend') }}/assets/demo/datatables-demo.js"></script>  -->
@@ -172,6 +177,8 @@
                 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
                 <script src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.html5.min.js"></script>
                 <script src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.colVis.min.js"></script>
+
+
 
         @yield('script')
     </body>
