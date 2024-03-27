c40471a6f5dd8a6566e0fbb6d45406fc0cd61ddd
jost3 <1427593@senati.pe>
2024-03-26 22:28:01 -0500

    login in process

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,95 +5,81 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Iniciar Sesión</title>
-    <link rel="stylesheet" href="{{ asset('css/styles.css') }}"> <!-- Enlazar tu archivo CSS -->
-    <link rel="stylesheet" href="{{ asset('css/stylosredes.css') }}">
-    <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="{{ asset('css/') }}">
-    
+    <!-- Enlazar tu archivo CSS -->
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+
+   
+  
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.6.0/mdb.min.css" rel="stylesheet">
+    <style>
+        body {
+            background-color: #f5f5f5;
+        }
+
+        .form-outline {
+            border: 1px solid #d2d2d2;
+        }
+
+        .form-label {
+            color: #495057;
+        }
+    </style>
 </head>
 
 <body>
     <div class="container">
-        <div class="forms-container">
-            <div class="signin-signup">
-                <div class="card">
+        <div class="row">
+            <div class="col-sm-6 text-black">
+                <div class="px-5 ms-xl-4">
+                    <i class="fas fa-crow fa-2x me-3 pt-5 mt-xl-4" style="color: #709085;"></i>
+                    <span class="h1 fw-bold mb-0">Logo</span>
+                </div>
+
+                <div class="d-flex align-items-center h-custom-2 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5">
                     <form action="{{ url('/loginapi') }}" method="POST" class="sign-in-form">
                         @csrf
-                        <h2 class="title">Inicio de sesión</h2>
                         @if ($errors->any())
                         <div id="error-alert" class="custom-alert">
                             {{ $errors->first('message') }}
                         </div>
                         @endif
-                        <div class="input-field">
-                            <i class="fas fa-user"></i>
-                            <input type="email" name="email" placeholder="Email" required />
+
+                        <h3 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Log in</h3>
+                        <div class="form-outline mb-4">
+                           
+                            <input type="email" name="email" id="form2Example18" class="form-control form-control-lg" />
+                            <label class="form-label" for="form2Example18">Email address</label>
                         </div>
-                        <div class="input-field">
-                            <i class="fas fa-lock"></i>
-                            <input type="password" name="password" id="password" placeholder="Password" required />
-                            <i id="togglePassword" class="fas fa-eye" style="position: absolute; right: 10px; top: 50%; transform: translateY(-50%); cursor: pointer;"></i>
+                        <div class="form-outline mb-4">
+                            
+                            <input type="password" name="password" id="form2Example28" class="form-control form-control-lg" />
+                            <label class="form-label" for="form2Example28">Password</label>
                         </div>
-                        <input type="submit" value="Login" class="btn solid" />
+                        <div class="pt-1 mb-4">
+                            <button class="btn btn-info btn-lg btn-block" type="submit">Login</button>
+                        </div>
+                        <p class="small mb-5 pb-lg-2"><a class="text-muted" href="#!">Forgot password?</a></p>
+                        <p>Don't have an account? <a href="#!" class="link-info">Register here</a></p>
                     </form>
                 </div>
-                <div class="redes-sociales">
-                    <p></p>
-                    <ul>
-                        <li><a href="https://www.facebook.com/"><i class="fab fa-facebook"></i></a></li>
-                        <li><a href="https://www.tiktok.com/"><i class="fab fa-tiktok"></i></a></li>
-                        <li><a href="https://www.youtube.com/"><i class="fab fa-youtube"></i></a></li>
-                        <li><a href="https://twitter.com/"><i class="fab fa-twitter"></i></a></li>
-                        <li><a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a></li>
-                    </ul>
-                </div>
             </div>
-        </div>
-        <div class="panels-container">
-            <div class="panel left-panel">
-                <div class="content">
-                    <h3>¡Sistema Mjj_E_Store_System!</h3>
-                    <p>Descubre un mundo de posibilidades infinitas para tu negocio. Únete a nuestra comunidad hoy mismo y comienza tu viaje hacia el éxito. ¡No esperes más para hacer realidad tus sueños empresariales!</p>
-                </div>
-                <img src="{{ asset('images/log.png') }}" class="image" alt="">
-            </div>
-            <div class="panel right-panel">
-                <div class="content">
-                    <h3></h3>
-                    <p></p>
-                </div>
-                <img src="./img/register.svg" class="image" alt="">
+            <div class="col-sm-6 px-0 d-none d-sm-block">
+                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img3.webp"
+                    alt="Login image" class="w-100 vh-100" style="object-fit: cover; object-position: left;">
             </div>
         </div>
     </div>
-    <script src="{{ asset('js/app.js') }}"></script>
-    <script src="{{ asset('css/alerta.js') }}"></script>
-    <script src="./app.js"></script>
-    <script src="./alerta.js"></script>
-    <script>
-        var passwordInput = document.getElementById('password');
-        var togglePassword = document.getElementById('togglePassword');
-
-        togglePassword.addEventListener('click', function () {
-            if (passwordInput.type === 'password') {
-                passwordInput.type = 'text';
-                togglePassword.classList.remove('fa-eye');
-                togglePassword.classList.add('fa-eye-slash');
-            } else {
-                passwordInput.type = 'password';
-                togglePassword.classList.remove('fa-eye-slash');
-                togglePassword.classList.add('fa-eye');
-            }
-        });
-
-        var errorAlert = document.getElementById('error-alert');
+</body>
+<script>
+    var errorAlert = document.getElementById('error-alert');
         if (errorAlert) {
             setTimeout(function () {
                 errorAlert.style.display = 'none';
             }, 2000);
         }
-    </script>
-</body>
-
+</script>
 </html>
+
 
